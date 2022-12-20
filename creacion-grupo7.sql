13364d84530199dd1bd55d9a121595830b07d7b1
Zegameus YT <30315974+Zegameus@users.noreply.github.com>
2022-12-20 09:54:01 +0100

    Create table Paises

--- creacion-grupo7.sql
+++ creacion-grupo7.sql
@@ -3,8 +3,10 @@
 CREATE DATABASE Triatlon; #Crear la base de datos de Triatlon
 
 CREATE TABLE Paises (
-    Cod_pais CHAR(3) constraint paises_pk primary key,
-    Nombre VARCHAR(20) constraint paises_uk1 unique constraint paises_nn1 not null
+    Cod_pais CHAR(3),
+    Nombre VARCHAR(20) not null,
+    constraint paises_pk PRIMARY KEY (Cod_pais),
+    constraint paises_uk1 unique (Nombre)
 );
 #CREATE TABLE Atletas ();
 #CREATE TABLE Campeonatos();
