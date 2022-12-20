e0385336a1a3be8eb706da8a69ccb1ee57beba1c
jairoobispo <obispoperezjairo@gmail.com>
2022-12-20 10:05:47 +0100

    Create table atletas

--- creacion-grupo7.sql
+++ creacion-grupo7.sql
@@ -8,6 +8,18 @@ CREATE TABLE Paises (
     constraint paises_pk PRIMARY KEY (Cod_pais),
     constraint paises_uk1 unique (Nombre)
 );
-#CREATE TABLE Atletas ();
+CREATE TABLE Atletas (
+    Id_atleta smallint(255),
+    Nombre varchar(30) not null,
+    Apellido varchar(30) not null,
+    Pais char(3),
+    Anno_nacimiento date,
+    Entrenador varchar(25),
+    World_ranking smallint(255),
+    Continental_ranking smallint(255),
+    constraint atletas_pk primary key (Id_atleta),
+    constraint atletas_uk1 unique (Nombre),
+    constraint atletas_fk1 foreign key (Pais) references Paises(Cod_pais)
+);
 #CREATE TABLE Campeonatos();
 #CREATE TABLE Resultados();
