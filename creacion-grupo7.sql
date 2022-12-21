1892fe05d739b1396f4a5cea26bb354fadeec80d
Zegameus YT <enriquecali43ff@gmail.com>
2022-12-21 13:06:52 +0100

    Cambiar tipos de datos básicos

--- creacion-grupo7.sql
+++ creacion-grupo7.sql
@@ -7,36 +7,37 @@ GRANT ALL PRIVILEGES ON triatlon.* TO Azpeleta;
 
 -- con un usario que no sea root
 CREATE TABLE Paises (
-    Cod_pais CHAR(3),
+    Cod_Pais CHAR(3), #ESP,GER,GBR
     Nombre VARCHAR(20) not null,
-    constraint paises_pk PRIMARY KEY (Cod_pais),
+    constraint paises_pk PRIMARY KEY (Cod_Pais),
     constraint paises_uk1 unique (Nombre)
 );
 CREATE TABLE Atletas (
-    Id_atleta smallint(255),
+    Id_atleta smallint(5) unsigned,
     Nombre varchar(30) not null,
     Apellido varchar(30) not null,
-    Pais char(3),
+    Cod_Pais char(3),
     Anno_nacimiento date,
     Entrenador varchar(25),
-    World_ranking smallint(255),
-    Continental_ranking smallint(255),
+    World_ranking smallint(5) unsigned,
+    Continental_ranking smallint(5) unsigned,
+
     constraint atletas_pk primary key (Id_atleta),
     constraint atletas_uk1 unique (Nombre),
-    constraint atletas_fk1 foreign key (Pais) references Paises(Cod_pais)
+    constraint atletas_fk1 foreign key (Cod_Pais) references Paises(Cod_Pais)
 );
 CREATE TABLE Campeonatos(
-    id_campeonato SMALLINT(255),
+    id_campeonato SMALLINT(5) unsigned,
     ano YEAR NOT NULL,
     categoría VARCHAR(10) NOT NULL ,
     continente VARCHAR(8) NOT NULL ,
-    país       CHAR(3),
+    pais       CHAR(3),
     CONSTRAINT campeonatos_pk PRIMARY KEY (id_campeonato),
-    CONSTRAINT campeonatos_fk1 FOREIGN KEY (país) REFERENCES Paises(cod_pais)
+    CONSTRAINT campeonatos_fk1 FOREIGN KEY (pais) REFERENCES Paises(cod_pais)
 );
 CREATE TABLE Resultados(
-    id_campeonato smallint(255),
-    id_atleta smallint(255),
+    id_campeonato smallint(5) unsigned,
+    id_atleta smallint(5) unsigned,
     posicion_final
 );
 GRANT ALL PRIVILEGES ON triatlon.* TO Azpeleta;
