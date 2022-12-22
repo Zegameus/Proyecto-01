84d467ccd913dc4563a5ecea4b082aea4311d402
Zegameus <enriquecali43ff@gmail.com>
2022-12-22 22:39:36 +0100

    Actualización sobre tipos y añadir info.

--- Insertar-info.sql
+++ Insertar-info.sql
@@ -1,5 +1,22 @@
+USE triatlon;
 -- Insertar Paises
-INSERT INTO paises values ('AFG','Afganistan'),('ALB','Albania'),('ALG','Algeria'),('ASA','Samoa Americana'),('AND','Andorra'),('ANG','Angola'),('AIA','Anguila'),('ANT','Anigua y Barbuda'),('ARG','Argentina'),('ARM','Armenia'),('ARU','Aruba'),('ANZ','Australasia'),('AUS','Australia'),('AUT','Austria'),('AZE','Azerbaiyán');
+-- A
+INSERT INTO paises values ('AFG','Afganistan'),
+                          ('ALB','Albania'),
+                          ('ALG','Algeria'),
+                          ('ASA','Samoa Americana'),
+                          ('AND','Andorra'),
+                          ('ANG','Angola'),
+                          ('AIA','Anguila'),
+                          ('ANT','Anigua y Barbuda'),
+                          ('ARG','Argentina'),
+                          ('ARM','Armenia'),
+                          ('ARU','Aruba'),
+                          ('ANZ','Australasia'),
+                          ('AUS','Australia'),
+                          ('AUT','Austria'),
+                          ('AZE','Azerbaiyán');
+-- B
 INSERT INTO paises values ('BAH','Bahamas'),
                           ('BRN','Baréin'),
                           ('BAN','Bangladesh'),
@@ -19,16 +36,43 @@ INSERT INTO paises values ('BAH','Bahamas'),
                           ('BUL','Bulgaria'),
                           ('BUR','Brukina Faso'),
                           ('BDI','Burundi');
+-- C
 INSERT INTO paises values ('CAM','Cambodia'),
                           ('CMR','Camerún'),
                           ('CAN','Canadá');
+-- E
+-- F
+INSERT INTO paises values ('FIJ','Fiji');
+-- G
+INSERT INTO paises VALUES ('GER','Alemania');
+-- H
+-- I
+-- J
+-- K
+-- L
+-- M
+-- N
+-- O
+-- P
+-- Q
+-- R
+-- S
+INSERT INTO paises VALUES ('SAM', 'Samoa'),('SOL', 'Islas Salomon'),('SWE','Suecia');
+-- T
+-- U
+-- V W X Y Z
 
 -- Atletas
-
+INSERT INTO atletas VALUES (1,'Jett Lee','Curteis','SAM',2005,NULL,502,43,NULL,NULL,1,1,NULL,NULL,NULL),
+                           (2,'Nikotimasi Tu''enadi','Croker','FIJ',2004,NULL,515,46,NULL,NULL,1,0,NULL,NULL,NULL);
+INSERT INTO atletas value (3,'Christian Zadkiel','Rokoua','FIJ',2006,NULL,NULL,NULL,NULL,NULL,1,0,NULL,NULL,NULL);
+INSERT INTO atletas VALUES (4, 'Gabriel','Sandör','SWE',1996,NULL,120,65,NULL,NULL,18,12,'@SandorGabriel','@gabrielsandor','GabrielSandorTri'),
+                           (5,'Justus','Nieschlag','GER',1992,'Dan Lorang',116,63,NULL,NULL,17,9,'@JustusNieschlag','@justus.nieschlag','JustusNieschlag');
 -- Entrenadores
-
+select *
+from atletas;
 -- Campeonatos
-
+INSERT INTO campeonatos(id_campeonato, anno, continente, pais) VALUES (1,2022,'Oceania','FIJ');
 -- Programas
 
 -- Jurados
--- creacion-grupo7.sql
+++ creacion-grupo7.sql
@@ -1,6 +1,7 @@
 # Authors: Azpeleta, David; García, Enrique; Obispo, Jairo
 
-CREATE DATABASE Triatlon; #Crear la base de datos de Triatlon (Con el usuario Root)
+CREATE DATABASE triatlon; #Crear la base de datos de Triatlon (Con el usuario Root)
+USE triatlon; -- Conectarse al esquema triatlon
 GRANT ALL PRIVILEGES ON triatlon.* TO Zegameus; -- Dar permisos al usuario Zegameus en la base de datos del Triatlon
 GRANT ALL PRIVILEGES ON triatlon.* TO Ja1Rin;
 GRANT ALL PRIVILEGES ON triatlon.* TO Azpeleta;
@@ -8,84 +9,87 @@ GRANT ALL PRIVILEGES ON triatlon.* TO Azpeleta;
 -- con un usario que no sea root
 CREATE TABLE Paises (
     Cod_Pais CHAR(3), #ESP,GER,GBR
-    Nombre VARCHAR(30) not null,
-    constraint paises_pk PRIMARY KEY (Cod_Pais),
-    constraint paises_uk1 unique (Nombre),#Combinado con el not null, se convierte en
-    constraint paises_ck1 check ( Cod_Pais != '' AND Nombre != '' )
+    Nombre VARCHAR(30) NOT NULL,
+    CONSTRAINT paises_PK PRIMARY KEY (Cod_Pais),
+    CONSTRAINT paises_UK1 UNIQUE (Nombre),#Combinado con el NOT NULL, se convierte en
+    CONSTRAINT paises_CK1 CHECK ( Cod_Pais != '' AND Nombre != '' )
 );
 
 CREATE TABLE Atletas (
-    Id_atleta smallint(5) unsigned,
-    Nombre varchar(30) not null,
-    Apellido varchar(30) not null,
-    Cod_Pais char(3) not null,
-    Anno_nacimiento date not null,
-    Entrenador varchar(25), #Hay atletas que no tienen un entrenador (o por lo menos no de forma pública)
-    World_ranking smallint(5) unsigned not null,
-    Continental_ranking smallint(5) unsigned not null,
-    Foto blob,
-    Biografia longtext, # Hay atletas que no han dado una biografía
-    N_Veces_Podio smallint(3) unsigned default 0 not null, #Hay atletas que no han estado en el podio
-    N_Victorias smallint(3) unsigned default 0 not null, # Hay atletas que no han ganado nunca
+    Id_atleta SMALLINT(5) UNSIGNED,
+    Nombre VARCHAR(30) NOT NULL,
+    Apellido VARCHAR(30) NOT NULL,
+    Cod_Pais CHAR(3) NOT NULL,
+    Anno_nacimiento YEAR NOT NULL,
+    Entrenador VARCHAR(25), #Hay atletas que no tienen un entrenador (o por lo menos no de forma pública)
+    World_ranking SMALLINT(5) UNSIGNED,
+    Continental_ranking SMALLINT(5) UNSIGNED,
+    Foto BLOB,
+    Biografia LONGTEXT, # Hay atletas que no han dado una biografía
+    N_Veces_Podio SMALLINT(3) UNSIGNED DEFAULT 0 NOT NULL, #Hay atletas que no han estado en el podio
+    N_Victorias SMALLINT(3) UNSIGNED DEFAULT 0 NOT NULL, # Hay atletas que no han ganado nunca
     Twitter VARCHAR(30),
     Instagram VARCHAR(30),
     Facebook VARCHAR(30),
-    constraint atletas_pk primary key (Id_atleta),
-    constraint atletas_fk1 foreign key (Cod_Pais) references Paises(Cod_Pais) on delete cascade ,
-    constraint atletas_uk1 unique (World_ranking), #Solo una persona puede estar en un puesto concreto
-    constraint atletas_uk2 unique (Continental_ranking) #Lo mismo que pasa con el anterior
+    CONSTRAINT atletas_PK PRIMARY KEY (Id_atleta),
+    CONSTRAINT atletas_FK1 FOREIGN KEY (Cod_Pais) REFERENCES Paises(Cod_Pais) ON DELETE CASCADE ,
+    CONSTRAINT atletas_UK1 UNIQUE (World_ranking), -- Solo una persona puede estar en un puesto concreto
+    CONSTRAINT atletas_UK2 UNIQUE (Continental_ranking), -- Lo mismo que pasa con el anterior
+    CONSTRAINT atletas_CK1 CHECK ( Nombre != '' AND Apellido != '' ) -- Esto es para evitar introducir datos vacios
 );
 CREATE TABLE Campeonatos(
-    id_campeonato SMALLINT(5) unsigned,
+    id_campeonato SMALLINT(5) UNSIGNED,
     anno YEAR NOT NULL,
     continente VARCHAR(8) NOT NULL,
     pais       CHAR(3),
-    CONSTRAINT campeonatos_pk PRIMARY KEY (id_campeonato),
-    CONSTRAINT campeonatos_fk1 FOREIGN KEY (pais) REFERENCES Paises(cod_pais) on delete cascade
+    CONSTRAINT campeonatos_PK PRIMARY KEY (id_campeonato),
+    CONSTRAINT campeonatos_FK1 FOREIGN KEY (pais) REFERENCES Paises(cod_pais) ON DELETE CASCADE,
+    CONSTRAINT campeonatos_CK1 CHECK ( continente != '' )
 );
 #Procedemos con los diferentes programas que tiene un campeonato, la sección femenina/masculina de elite, jovenes, U15...
 create table Programa (
-    Id_Campeonato smallint(5) unsigned,
-    Id_Programa smallint(5) unsigned,
-    Technical_delegates varchar(40),
-    Refree varchar(20), #árbitro
-    Distancia_Natacion smallint(40) unsigned,
-    Distancia_Bicicleta smallint(40) unsigned,
-    Distancia_Carrera smallint(40) unsigned,
-    Temp_Aire tinyint(2) unsigned,#En grados centigrados de 0 a 99º
-    Temp_Agua tinyint(2) unsigned,
-    constraint programa_pk PRIMARY KEY (Id_Campeonato,Id_Programa),
-    constraint programa_fk1 FOREIGN KEY (Id_Campeonato) REFERENCES Campeonatos(id_campeonato) on delete cascade
+    Id_Campeonato SMALLINT(5) UNSIGNED,
+    Id_Programa SMALLINT(5) UNSIGNED,
+    Technical_delegates VARCHAR(40),
+    Refree VARCHAR(20), #árbitro
+    Distancia_Natacion SMALLINT(40) UNSIGNED,
+    Distancia_Bicicleta SMALLINT(40) UNSIGNED,
+    Distancia_Carrera SMALLINT(40) UNSIGNED,
+    Temp_Aire TINYINT(2) UNSIGNED,#En grados centigrados de 0 a 99º
+    Temp_Agua TINYINT(2) UNSIGNED,
+    CONSTRAINT programa_PK PRIMARY KEY (Id_Campeonato,Id_Programa),
+    CONSTRAINT programa_FK1 FOREIGN KEY (Id_Campeonato) REFERENCES Campeonatos(id_campeonato) ON DELETE CASCADE
 
 );
 
 CREATE TABLE Resultados(
-    id_campeonato smallint(5) unsigned,
-    id_programa smallint(5) unsigned,
-    id_atleta smallint(5) unsigned,
-    posicion_final char(3), #Puede ser un numero o DNF (no ha terminado), DNS (No empezó), DSQ (Descalificado)...
-    tiempo_natacion time,
-    tiempo_bici time,
-    tiempo_carrera time,
-    constraint resultados_pk PRIMARY KEY (id_campeonato,id_programa,id_atleta),
-    constraint resultados_fk1 FOREIGN KEY (id_atleta) REFERENCES Atletas(Id_atleta) on delete cascade,
-    constraint resultados_fk2 FOREIGN KEY (id_campeonato) REFERENCES Programa(Id_Campeonato) on delete cascade
+    id_campeonato SMALLINT(5) UNSIGNED,
+    id_programa SMALLINT(5) UNSIGNED,
+    id_atleta SMALLINT(5) UNSIGNED,
+    posicion_final CHAR(3), #Puede ser un numero o DNF (no ha terminado), DNS (No empezó), DSQ (Descalificado)...
+    tiempo_natacion TIME,
+    tiempo_bici TIME,
+    tiempo_carrera TIME,
+    CONSTRAINT resultados_PK PRIMARY KEY (id_campeonato,id_programa,id_atleta),
+    CONSTRAINT resultados_FK1 FOREIGN KEY (id_atleta) REFERENCES Atletas(Id_atleta) ON DELETE CASCADE,
+    CONSTRAINT resultados_FK2 FOREIGN KEY (id_campeonato,id_programa) REFERENCES Programa(Id_Campeonato,Id_Programa) ON DELETE CASCADE,
+    CONSTRAINT resultados_CK1 CHECK ( posicion_final >= 0 AND posicion_final <= 999 OR posicion_final = 'DNF' OR posicion_final = 'DNS' OR posicion_final = 'DSQ' )
 );
 
 CREATE TABLE Entrenadores_atleta(
-    Id_atleta smallint(5) unsigned,
-    Entrenador varchar(25),
-    CONSTRAINT Entrenadores_atleta_pk PRIMARY KEY (Id_atleta,Entrenador),
-    constraint entrenadores_atleta_fk1 FOREIGN KEY (Id_atleta) REFERENCES Atletas(Id_atleta) on delete cascade
+    Id_atleta SMALLINT(5) UNSIGNED,
+    Entrenador VARCHAR(25),
+    CONSTRAINT Entrenadores_atleta_PK PRIMARY KEY (Id_atleta,Entrenador),
+    CONSTRAINT entrenadores_atleta_FK1 FOREIGN KEY (Id_atleta) REFERENCES Atletas(Id_atleta) ON DELETE CASCADE
 );
 
 CREATE TABLE jurado(
-    id_campeonato smallint(5) unsigned,
-    id_programa smallint(5) unsigned,
-    jurado      varchar(25),
-    Cod_Pais    char(3),
-    CONSTRAINT jurado_pk PRIMARY KEY (id_campeonato,id_programa,jurado),
-    CONSTRAINT jurado_fk1 FOREIGN KEY (id_campeonato,id_programa) REFERENCES Programa(Id_Campeonato,id_programa) on delete cascade
+    id_campeonato SMALLINT(5) UNSIGNED,
+    id_programa SMALLINT(5) UNSIGNED,
+    jurado      VARCHAR(25),
+    Cod_Pais    CHAR(3),
+    CONSTRAINT jurado_PK PRIMARY KEY (id_campeonato,id_programa,jurado),
+    CONSTRAINT jurado_FK1 FOREIGN KEY (id_campeonato,id_programa) REFERENCES Programa(Id_Campeonato,id_programa) ON DELETE CASCADE
 );
 
 SHOW FULL TABLES FROM Triatlon; -- Ver tablas creadas
