346a43b3d809ca2b28f85aa047365b9cd4a2a01d
Zegameus YT <enriquecali43ff@gmail.com>
2022-12-22 09:42:39 +0100

    Agregar contenido a la base de datos

---
+++ Insertar-info.sql
@@ -0,0 +1,16 @@
+-- Insertar Paises
+INSERT INTO paises values ('ESP','España'),('GER','Alemania'),('NZL','Nueva Zelanda'),('EGY','Egipto'),('BRA','Brasil');
+INSERT INTO paises values ('POL','Polonia'),('GBR','Gran Bretaña'),('USA','Estados Unidos de Ámerica'),('RSA','Sudafrica'),('SVK','Eslovaquia');
+INSERT INTO paises values ('HKG','HongKong'),('SGP','Singapur'),('THA','Thailandia'),('FIJ','Fiji'),('','');
+INSERT INTO paises values ('AFG','Afganistan'),('ALB','Albania'),('ALG','Algeria'),('ASA','Samoa Americana'),('AND','Andorra'),('ANG','Angola'),('AIA','Anguila'),('ANT','Anigua y Barbuda'),('ARG','Argentina'),('ARM','Armenia'),('ARU','Aruba'),('ANZ','Australasia'),('AUS','Australia'),('AUT','Austria'),('AZE','Azerbaiyán');
+-- Atletas
+
+-- Entrenadores
+
+-- Campeonatos
+
+-- Programas
+
+-- Jurados
+
+-- Resultados
