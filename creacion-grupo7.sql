378a1cb27a63444dcf8be955ac4bdd6c7527cff5
Ja1Rin <obispoperezjairo@gmail.com>
2022-12-26 13:14:18 +0100

    ACTUALIZACION TABLAS

--- creacion-grupo7.sql
+++ creacion-grupo7.sql
@@ -25,7 +25,7 @@ CREATE TABLE Paises (
 );
 
 CREATE TABLE Atletas (
-    Id_atleta SMALLINT(5) UNSIGNED,
+    Id_atleta DECIMAL(4) UNSIGNED,
     Nombre VARCHAR(30) NOT NULL,
     Segundo_Nombre varchar(30),
     Apellido VARCHAR(30) NOT NULL,
@@ -34,8 +34,8 @@ CREATE TABLE Atletas (
     Entrenador VARCHAR(25), #Hay atletas que no tienen un entrenador (o por lo menos no de forma pública)
     Foto BLOB,
     Biografia LONGTEXT, # Hay atletas que no han dado una biografía
-    N_Veces_Podio SMALLINT(3) UNSIGNED DEFAULT 0 NOT NULL, #Hay atletas que no han estado en el podio
-    N_Victorias SMALLINT(3) UNSIGNED DEFAULT 0 NOT NULL, # Hay atletas que no han ganado nunca
+    N_Veces_Podio DECIMAL(2) UNSIGNED DEFAULT 0 NOT NULL, #Hay atletas que no han estado en el podio
+    N_Victorias DECIMAL(2) UNSIGNED DEFAULT 0 NOT NULL, # Hay atletas que no han ganado nunca
     Twitter VARCHAR(30),
     Instagram VARCHAR(30),
     Facebook VARCHAR(30),
@@ -44,34 +44,34 @@ CREATE TABLE Atletas (
     CONSTRAINT atletas_CK1 CHECK ( Nombre != '' AND Apellido != '' ) -- Esto es para evitar introducir datos vacios
 );
 CREATE TABLE Campeonatos(
-    id_campeonato SMALLINT(5) UNSIGNED,
+    id_campeonato DECIMAL(4) UNSIGNED,
     anno YEAR NOT NULL,
     ID_cont DECIMAL(1) NOT NULL,
-    pais       CHAR(3),
+    cod_pais       CHAR(3),
     CONSTRAINT campeonatos_PK PRIMARY KEY (id_campeonato),
-    CONSTRAINT campeonatos_FK1 FOREIGN KEY (pais) REFERENCES Paises(cod_pais) ON DELETE CASCADE,
+    CONSTRAINT campeonatos_FK1 FOREIGN KEY (cod_pais) REFERENCES Paises(cod_pais) ON DELETE CASCADE,
     CONSTRAINT campeonatos_FK2 FOREIGN KEY (ID_cont) REFERENCES Continentes(ID_cont)
 );
 #Procedemos con los diferentes programas que tiene un campeonato, la sección femenina/masculina de elite, jovenes, U15...
 create table Programa (
-    Id_Campeonato SMALLINT(5) UNSIGNED,
-    Id_Programa SMALLINT(5) UNSIGNED,
+    Id_Campeonato DECIMAL(4) UNSIGNED,
+    Id_Programa DECIMAL(4) UNSIGNED,
     Technical_delegates VARCHAR(40),
     Refree VARCHAR(20), #árbitro
-    Distancia_Natacion SMALLINT(40) UNSIGNED,
-    Distancia_Bicicleta SMALLINT(40) UNSIGNED,
-    Distancia_Carrera SMALLINT(40) UNSIGNED,
-    Temp_Aire TINYINT(2) UNSIGNED,#En grados centigrados de 0 a 99º
-    Temp_Agua TINYINT(2) UNSIGNED,
+    Distancia_Natacion DECIMAL(4) UNSIGNED,
+    Distancia_Bicicleta DECIMAL(2) UNSIGNED,
+    Distancia_Carrera DECIMAL(2,1) UNSIGNED,
+    Temp_Aire DECIMAL(3,2) UNSIGNED,#En grados centigrados de 0 a 99º
+    Temp_Agua DECIMAL(3,2) UNSIGNED,
     CONSTRAINT programa_PK PRIMARY KEY (Id_Campeonato,Id_Programa),
     CONSTRAINT programa_FK1 FOREIGN KEY (Id_Campeonato) REFERENCES Campeonatos(id_campeonato) ON DELETE CASCADE
 
 );
 
 CREATE TABLE Resultados(
-    id_campeonato SMALLINT(5) UNSIGNED,
-    id_programa SMALLINT(5) UNSIGNED,
-    id_atleta SMALLINT(5) UNSIGNED,
+    id_campeonato DECIMAL(4) UNSIGNED,
+    id_programa DECIMAL(5) UNSIGNED,
+    id_atleta DECIMAL(5) UNSIGNED,
     posicion_final CHAR(3), #Puede ser un numero o DNF (no ha terminado), DNS (No empezó), DSQ (Descalificado)...
     tiempo_natacion TIME,
     tiempo_bici TIME,
@@ -83,15 +83,15 @@ CREATE TABLE Resultados(
 );
 
 CREATE TABLE Entrenadores_atleta(
-    Id_atleta SMALLINT(5) UNSIGNED,
-    Entrenador VARCHAR(25),
+    Id_atleta DECIMAL(5) UNSIGNED,
+    Entrenador VARCHAR(30),
     CONSTRAINT Entrenadores_atleta_PK PRIMARY KEY (Id_atleta,Entrenador),
     CONSTRAINT entrenadores_atleta_FK1 FOREIGN KEY (Id_atleta) REFERENCES Atletas(Id_atleta) ON DELETE CASCADE
 );
 
 CREATE TABLE jurado(
-    id_campeonato SMALLINT(5) UNSIGNED,
-    id_programa SMALLINT(5) UNSIGNED,
+    id_campeonato DECIMAL(4) UNSIGNED,
+    id_programa DECIMAL(5) UNSIGNED,
     jurado      VARCHAR(25),
     Cod_Pais    CHAR(3),
     CONSTRAINT jurado_PK PRIMARY KEY (id_campeonato,id_programa,jurado),
